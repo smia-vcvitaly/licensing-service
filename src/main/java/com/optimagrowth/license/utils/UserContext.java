124b1077ba703c77d85ff1fbfa9692b7dcd48896
Vitalii Chura <vcvitaly@yahoo.com>
2022-12-01 05:28:11 +0200

    complete ch8

--- src/main/java/com/optimagrowth/license/utils/UserContext.java
+++ src/main/java/com/optimagrowth/license/utils/UserContext.java
@@ -14,10 +14,10 @@ public class UserContext {
     public static final String AUTH_TOKEN = "tmx-auth-token";
     public static final String USER_ID = "tmx-user-id";
     public static final String ORGANIZATION_ID = "tmx-organization-id";
-    private String correlationId= new String();
-    private String authToken= new String();
-    private String userId = new String();
-    private String organizationId = new String();
+    private String correlationId = "";
+    private String authToken = "";
+    private String userId = "";
+    private String organizationId = "";
     public String getCorrelationId() { return correlationId;}
     public void setCorrelationId(String correlationId) {
         this.correlationId = correlationId;
